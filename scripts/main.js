021755bb9be60fa0b04f956aaaeacae16d29bcd9
cagloria <33134245+cagloria@users.noreply.github.com>
2020-03-02 22:11:47 -0700

    Mobile nav resets when window passes mobile width
    Added listener to window to close an open nav and reset the icon for the nav button if the window is resized beyond mobile layout

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,17 @@
+/**
+ * Toggle class to open the mobile navigation and switch the nav button icon.
+ */
 function toggleNav() {
     $("#nav-list").toggleClass("nav-list--open");
     $("#nav-button").toggleClass("nav-button--open-nav");
 }
+
+window.addEventListener("resize", function() {
+    let $navList = $("#nav-list");
+    let $navButton = $("#nav-button");
+
+    if (window.innerWidth > 900) {
+        $navList.removeClass("nav-list--open");
+        $navButton.removeClass("nav-button--open-nav");
+    }
+});
